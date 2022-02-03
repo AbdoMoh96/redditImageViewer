8959a7cd03da8801126347b0283f890156ef4a74
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2022-02-03 13:52:09 +0200

    added mini gif functionality

--- src/Helpers/imagesGetter.js
+++ src/Helpers/imagesGetter.js
@@ -3,11 +3,20 @@ const imagesGetter = (images) => {
     return  images.map((element) => {
             let image = null;
             if (element.data.post_hint === "image") {
-                image = {
-                    id: element.data.name,
-                    title: element.data.title,
-                    url: element.data.preview.images[0].source.url.replace("amp;", ""),
-                };
+               let gifUrl = element.data.url_overridden_by_dest;
+               if (gifUrl.includes('gif')){
+                   image = {
+                       id: element.data.name,
+                       title: element.data.title,
+                       url: gifUrl,
+                   };
+               }else{
+                   image = {
+                       id: element.data.name,
+                       title: element.data.title,
+                       url: element.data.preview.images[0].source.url.replace("amp;", ""),
+                   };
+               }
             }
             return image;
         })
