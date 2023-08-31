08ee65260b58f856f4139c12339777bf79af8730
Abdelrahman Mohammed <window.2015@hotmail.com>
2023-09-01 01:53:24 +0300

    removed gifs

--- src/Helpers/imagesGetter.js
+++ src/Helpers/imagesGetter.js
@@ -3,20 +3,11 @@ const imagesGetter = (images) => {
     return  images.map((element) => {
             let image = null;
             if (element.data.post_hint === "image") {
-               let gifUrl = element.data.url_overridden_by_dest;
-               if (gifUrl.includes('gif')){
-                   image = {
-                       id: element.data.name,
-                       title: element.data.title,
-                       url: gifUrl,
-                   };
-               }else{
                    image = {
                        id: element.data.name,
                        title: element.data.title,
                        url: element.data.url_overridden_by_dest
                    };
-               }
             }
             return image;
         })
