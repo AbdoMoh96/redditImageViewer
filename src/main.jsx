824b4436edb05aaa5dccf521b3b774bdb06e93d4
Abdelrahman Mohammed <abdo.moh96@outlook.com>
2026-01-04 19:47:13 +0200

    app works

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,5 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Router from "./Routes/Router";
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <Router/>
-    </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<Router/>)
 
